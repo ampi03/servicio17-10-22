8205627966e50fd4c71157ed2086468b79bb3e60
ampi03 <amparola03p@gmail.com>
2022-10-17 08:46:27 -0300

    primera parte

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { AdminComponent } from './pages/admin/admin.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NavbarComponent } from './pages/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactoComponent,
+    AdminComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,

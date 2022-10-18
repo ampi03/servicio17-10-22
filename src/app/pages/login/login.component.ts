582a50040f8804870a7db391719a0e722a6db2d1
ampi03 <amparola03p@gmail.com>
2022-10-18 10:05:06 -0300

    18/09

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { Usuarios } from 'src/app/modelos/usuarios';
+import { LoginService } from 'src/app/servicios/login.service';
 
 @Component({
   selector: 'app-login',
@@ -7,14 +9,25 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  colUsuarios: Usuarios[]=[]
 
-  datosUsuarios= new FormGroup ({
-    nombre: new FormControl,
-    contrasena: new FormControl,
-  })
-  constructor() { }
+  datosUsuarios= new FormGroup (
+    {
+    nombre: new FormControl('', Validators.required),
+    contrasena: new FormControl('', Validators.required)
+    }
+  )
+
+  constructor(private servicioUsuarios: LoginService) { }
 
   ngOnInit(): void {
+    this.servicioUsuarios.getUsuarios().subscribe(
+      usuarios => this.colUsuarios = usuarios
+    )
+  }
+
+  inicioSesion(){
+    this.servicioUsuarios.login(this.datosUsuarios, this.colUsuarios)
   }
 
 }
--- src/app/servicios/login.service.ts
+++ src/app/servicios/login.service.ts
@@ -2,19 +2,43 @@ import { Injectable } from '@angular/core';
 import { Usuarios } from '../modelos/usuarios';
 import { AngularFirestore, AngularFirestoreCollection } from "@angular/fire/compat/firestore";
 import { map } from 'rxjs';
+import { FormGroup } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
-
+  private isLoged = false;
   private coleccionUsuarios: AngularFirestoreCollection<Usuarios>
 
   constructor(private db: AngularFirestore) { 
     this.coleccionUsuarios = db.collection('usuarios');
   }
 
-  obtenerUsuarios(){
-    return this.coleccionUsuarios.snapshotChanges().pipe(map(action=>action.map(a=>a.payload.doc.data())))
+  getUsuarios(){
+    return this.coleccionUsuarios.snapshotChanges().pipe(
+      map(action=>action.map(a=>a.payload.doc.data()))
+    )
   }
+
+  login(form:FormGroup, usuariosCol: Usuarios[]){
+    let texto = "No inició"
+    if(form.valid){
+      usuariosCol.forEach(
+        usuario=>{
+          if(form.value.nombre === usuario.nombre){
+            if(form.value.contrasena === usuario.contrasena){
+              this.isLoged = true;
+              texto = "Inició sesión";
+            }
+          }
+        }
+      )
+      alert(texto);
+    }
+  }
+
+  estaLogueado(){
+    return this.isLoged
+  }  
 }
